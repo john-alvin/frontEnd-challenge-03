b3c9f19e9df1a7180a1f80a70e4e51dd3db44fc8
John Alvin <johnalvin.bonifacio@proton.me>
2022-09-09 02:16:21 +0800

    add body and btn-burger for open class

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 const menuBtn = document.querySelector(".menu-btn");
+const btnBurger = document.querySelector(".menu-btn__burger");
 const nav = document.querySelector(".nav");
+const body = document.querySelector("body");
 let menuOpen = false;
 
 menuBtn.addEventListener("click", function () {
   if (!menuOpen) {
+    body.classList.add("open");
     menuBtn.classList.add("open");
     nav.classList.add("open");
+    btnBurger.classList.add("open");
     menuOpen = true;
   } else {
+    body.classList.remove("open");
     menuBtn.classList.remove("open");
     nav.classList.remove("open");
+    btnBurger.classList.remove("open");
     menuOpen = false;
   }
 });
